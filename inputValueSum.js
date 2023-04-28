4c05c60f8072bef732884d3d738c46a74357c0bc
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-28 17:57:35 +0600

    Sum of two numbers in JavaScript

---
+++ inputValueSum.js
@@ -0,0 +1,9 @@
+let value_1 = prompt("Enter the 1st Number: ");
+let value_2 = prompt("Enter 2nd Number: ");
+
+value_1 = Number(value_1);
+value_2 = Number(value_2);
+
+let sum = value_1 + value_2;
+
+document.write("Sum of two Number is = " + sum);
